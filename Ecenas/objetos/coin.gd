59c47104db35ad75ac31d321a1c0302bd6a23035
Hecalfa <rivaspayes@gmail.com>
2022-08-19 14:16:41 -0600

    contador_de_monedas
    agregue el contador de monedas

--- Ecenas/objetos/coin.gd
+++ Ecenas/objetos/coin.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
+var booleana
+
 
 func _ready():
 	$AnimatedSprite.play("moneda")
 
 
-
 func _on_coin_body_entered(body):
 	if body.is_in_group("players"):
+		Global.contador += 1
+		get_node("CollisionShape2D").queue_free()
 		$AnimatedSprite.play("moneda_deah")
 		yield($AnimatedSprite, "animation_finished")
 		queue_free()
