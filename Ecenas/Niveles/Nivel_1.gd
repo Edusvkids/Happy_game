f532d3691c32a32d72eb764c6829fd812e03d5b4
Hecalfa <rivaspayes@gmail.com>
2022-10-08 18:11:16 -0600

    camara
    camara

--- Ecenas/Niveles/Nivel_1.gd
+++ Ecenas/Niveles/Nivel_1.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#onready var camara = $Player_woman/Camera2D2
 
+#export (int) var limite_c_up
+#export (int) var limite_c_down
+#export (int) var limite_c_left
+#export (int) var limite_c_rigth
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+	#camara.limit_left = limite_c_left
+	#camara.limit_right = limite_c_rigth
+	#camara.limit_top = limite_c_up
+	#camara.limit_bottom = limite_c_down
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
