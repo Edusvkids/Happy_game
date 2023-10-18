7af835c7b14262366d28aab47a2c3bdf3bb2d013
Hecalfa <rivaspayes@gmail.com>
2023-10-18 17:22:40 -0600

    ya esta el boton para ir al menu

--- Ecenas/Niveles/Nivel_1.gd
+++ Ecenas/Niveles/Nivel_1.gd
@@ -17,8 +17,14 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+func _physics_process(delta):
+	if Input.is_action_pressed("Inicio"):
+		get_tree().change_scene("res://Ecenas/Niveles/menu.tscn")
 
 func _on_el_matador_body_entered(body):
 	if body.is_in_group("players"):
 		Global.vidas -= 9999
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Ecenas/Niveles/menu.tscn")
